f8f33dc7559a6d64a7ad4362446072aa3d5dc9ca
Savitskiy <sovick90@bk.ru>
2022-09-18 16:22:41 +0300

    df

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
+const cors = require('./middlewares/cors');
+
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const routes = require('./routes');
@@ -10,7 +12,7 @@ const error = require('./middlewares/error');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-
+app.use(cors);
 app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
