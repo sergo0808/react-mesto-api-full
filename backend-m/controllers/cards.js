0dfb19a9223de7255c2f1c4ad67c193852baf764
Savitskiy <sovick90@bk.ru>
2022-10-17 11:41:49 +0300

    er

--- backend-m/controllers/cards.js
+++ backend-m/controllers/cards.js
@@ -5,6 +5,7 @@ const ForbiddenError = require('../errors/ForbiddenError');
 
 const getCards = (_, res, next) => {
   Card.find({})
+    .populate('user')
     .then((cards) => res.send(cards))
     .catch(next);
 };
