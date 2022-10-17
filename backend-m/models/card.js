5ba5884e045219d3b9c3dd7c2eec64e334a3c33e
Savitskiy <sovick90@bk.ru>
2022-10-17 11:47:50 +0300

    er

--- backend-m/models/card.js
+++ backend-m/models/card.js
@@ -21,13 +21,12 @@ const cardSchema = new mongoose.Schema({
     ref: 'user',
     required: true,
   },
-  likes: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'user',
-      default: [],
-    },
-  ],
+  likes: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    default: [],
+  },
+
   createdAt: {
     type: Date,
     default: Date.now,
