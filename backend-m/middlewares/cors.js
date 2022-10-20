b753afde58f764894426919c977ace59c436aa55
Savitskiy <sovick90@bk.ru>
2022-10-20 21:27:10 +0300

    er

--- backend-m/middlewares/cors.js
+++ backend-m/middlewares/cors.js
@@ -1,6 +1,6 @@
 const allowedCors = [
-  'http://mesto.frontend.sovickiy.nomoredomains.sbs/signin',
-  'https://mesto.frontend.sovickiy.nomoredomains.sbs/signin',
+  'http://mesto.frontend.sovickiy.nomoredomains.sbs',
+  'https://mesto.frontend.sovickiy.nomoredomains.sbs',
 ];
 
 const cors = (req, res, next) => {
@@ -11,7 +11,6 @@ const cors = (req, res, next) => {
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
   }
 
   if (method === 'OPTIONS') {
